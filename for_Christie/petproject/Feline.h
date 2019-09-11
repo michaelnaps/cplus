b302feb943752886c87981983a97973a9c2ac945
Michael Napoli <napoli.38@osu.edu>
2019-09-11 18:15:45 -0400

    time function completed

--- for_Christie/petproject/Feline.h
+++ for_Christie/petproject/Feline.h
@@ -89,7 +89,7 @@ private:
 		time_t current(time(0));  // current time
 		int elapsed(difftime(current, past_time));  // elapsed time from this session and last session
 		
-		iteration = (elapsed / 21600);  // cat loses one iteration in stats every six hours
+		iteration = (elapsed / 21600);  // cat loses one iteration in stats every SIX HOURS
 		
 		// the cat cannot die due to absence form the game
 		// if statements moderate the maximum integer the hunger and comfort variables can reach
