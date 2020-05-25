1b6bfc2313f058a67179cd4a71252954738d6fbe
Michael Napoli <napoli.38@osu.edu>
2020-05-24 22:16:21 -0400

    began work on coin class type

---
+++ my_includes/piggybank/coin.cpp
@@ -0,0 +1,71 @@
+// File: coin.cpp
+// Created by: Michael Napoli
+// Created on: 5/24/2020
+// Last modified on:
+
+/*
+   Purpose:
+*/
+
+#include "coin.h"
+#include <string>
+#include <iostream>
+
+namespace mercury {
+   // CONSTRUCTORS:
+   Coin::Coin() : name("INVALID NAME"), ind_value(-1), count(0) {}
+   Coin::Coin(std::string given_name, double amount) : count(0) {
+      name = given_name;
+      ind_value = amount;
+   }
+   Coin::Coin(std::string given_name, double amount, int num) {
+      name = given_name;
+      ind_value = amount;
+      count = num;
+   }
+
+
+   // GET MEMBER FUNCTIONS:
+   // return the number of coins of the specified type
+   int Coin::getCount() {
+      return count;
+   }
+
+   // return the current coin's given title
+   std::string Coin::getName() {
+      return name;
+   }
+
+   // return the value of an individual coin of the specified type
+   double Coin::getIndValue() {
+      return ind_value;
+   }
+
+   // return the total (based on coin value and number of coins)
+   double Coin::getTotal() {
+      return (count * ind_value);
+   }
+
+   // OPERATOR FUNCTIONS (FRIEND):
+   double operator+(Coin var1, Coin var2) {
+      return ((var1.count * var1.ind_value) + (var2.count * var2.ind_value));
+   }
+
+   double operator-(Coin var1, Coin var2) {
+      return ((var1.count * var1.ind_value) - (var2.count * var2.ind_value));
+   }
+
+   double operator++(Coin var) {
+      return ++var.count;
+   }
+
+   double operator--(Coin var) {
+      if (--var.count < 0) {
+         std::cout << "ERROR: No " << var.name << "(s) left to withdraw." << std::endl;
+         return 0;
+      }
+      else {
+         return --var.count;
+      }
+   }
+}
---
+++ my_includes/piggybank/coin.h
@@ -0,0 +1,43 @@
+// File: coin.h
+// Created by: Michael Napoli
+// Created on: 5/24/2020
+// Last modified on:
+
+/*
+   Purpose: Class for the declaration and organization of differing types of coins.
+*/
+
+#ifndef COIN_H
+#define COIN_H
+
+#include <string>
+
+namespace mercury {
+   class Coin
+   {
+   private:
+      int count;
+      std::string name;
+      double ind_value;
+
+   public:
+      // CONSTRUCTORS:
+      Coin();
+      Coin(std::string given_name, double amount);
+      Coin(std::string given_name, double amount, int num);
+
+      // GET FUNCTIONS:
+      int getCount();
+      std::string getName();
+      double getIndValue();
+      double getTotal();
+
+      // OPERATOR FUNCTIONS:
+      friend double operator+(Coin var1, Coin var2);
+      friend double operator-(Coin var1, Coin var2);
+      friend double operator++(Coin var);
+      friend double operator--(Coin var);
+   };
+}
+
+#endif
